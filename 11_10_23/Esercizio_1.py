9221f40cb8df31d611e0bfb88d6a1d0cdcaa3399
A-Kotchian <146761009+A-Kotchian@users.noreply.github.com>
2023-10-27 12:34:04 +0200

    Delete 11_10_23 directory

--- 11_10_23/Esercizio_1.py
+++
@@ -1,6 +0,0 @@
-i=0
-
-for f in range(0,101):
-    i=i+f
-
-print(i)
--- 11_10_23/Esercizio_2.py
+++
@@ -1,7 +0,0 @@
-i=input("Quanti numeri vuoi sommare?\n")
-s=0
-for i in range(0,int(i)+1):
-    s=s+i
-
-print("La somma dei primi {:} numeri è: {:}".format(i,s))
-
